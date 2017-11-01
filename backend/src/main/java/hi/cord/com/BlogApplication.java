17bebc782b62c24c5e9966fdc01acc369aa112e3
shun10114@gmail.com <shun10114@gmail.com>
2017-11-01 18:23:23 +0900

    [feature:rest] Adding Config about executors(Task, Scheduler)

--- backend/src/main/java/hi/cord/com/BlogApplication.java
+++ backend/src/main/java/hi/cord/com/BlogApplication.java
@@ -9,17 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
-import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.scheduling.concurrent.ScheduledExecutorFactoryBean;
-
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 //@EnableOAuth2Sso
@@ -43,20 +33,5 @@ public class BlogApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(BlogApplication.class);
         app.run();
-    }
-
-    //Cron Factory bean
-    @Bean
-    public ScheduledExecutorFactoryBean schedulerExecutor() {
-        ScheduledExecutorFactoryBean scheduledExecutorFactoryBean = new ScheduledExecutorFactoryBean();
-        scheduledExecutorFactoryBean.setPoolSize(10);
-        return scheduledExecutorFactoryBean;
-    }
-
-    // Need Test
-    @Bean("threadPoolExecutor")
-    public ThreadPoolExecutor threadPoolExecutor() {
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(500, 1000, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingDeque<>(), new ThreadPoolExecutor.DiscardOldestPolicy());
-        return threadPoolExecutor;
     }
 }
